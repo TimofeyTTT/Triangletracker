ae86a25dfc5d3a26af943d70c6b921ebb1a4c89f
timo <you@example.com>
2018-12-03 12:51:08 +0300

    Added new files

---
+++ README.md
@@ -0,0 +1,10 @@
+# triangle-tracker
+
+#Created by Timothy Ortega.
+
+### Triangle Tracker takes the lengths of three sides and determines if they form a valid triangle. If so, the tracker returns the name of the kind of triangle the sides make (equilateral, isosceles, scalene).
+------------------------------------------
+###A triangle is scalene if all of its three sides are different (in which case, the three angles are also different). If two of its sides are equal, a triangle is called isosceles. A triangle with all three equal sides is called equilateral.
+
+------------------------------------------
+![Triangle]()
---
+++ java/index.js
@@ -0,0 +1,20 @@
+ var sides=[];
+  function triangleType(sides){
+    var a = parseInt(document.getElementById('type').value);
+    var b= parseInt(document.getElementById('typeB').value);
+    var c= parseInt(document.getElementById('typeC').value);
+var sides=[];
+sides.push(a, b, c);
+var t = sides[0];
+var s = sides[1];
+var r = sides[2];
+      if(t===s && s===r && r===t){
+        alert("Equilateral Triangle");
+    } else if(t==s || r==s || r==t){
+      alert("Isosceles Triangle");
+    } else if(t!==s && s!==r && t!==r){
+        alert("Scalene Triangle");
+      }else{
+        alert("Not a Triangle");
+      }
+}
