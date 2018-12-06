2a9d9bba6118550c0f01eba961528eeeda50320f
TimofeyTTT <45222181+TimofeyTTT@users.noreply.github.com>
2018-12-06 11:20:23 +0300

    Update index.js

--- java/index.js
+++ java/index.js
@@ -1,5 +1,4 @@
- var sides=[];
-  function triangleType(sides){
+   function triangleType(sides){
     var a = parseInt(document.getElementById('type').value);
     var b= parseInt(document.getElementById('typeB').value);
     var c= parseInt(document.getElementById('typeC').value);
@@ -8,15 +7,15 @@ sides.push(a, b, c);
 var t = sides[0];
 var s = sides[1];
 var r = sides[2];
-      if(t===s && s===r && r===t){
+      if(t+s<=r || s+r<=t || t+r<=s){
+        alert("not a Triangle");
+      }else if(t===s && s===r && r===t){
         alert("Equilateral Triangle");
     } else if(t==s || r==s || r==t){
       alert("Isosceles Triangle");
     } else if(t!==s && s!==r && t!==r){
-        alert("Scalene Triangle")
-      }else if(t+s=>r || s+r=>t || t+r=>s){
-       alert("Not a Triangle")
+        alert("Scalene Triangle");
       }else{
-        alert("Not a Triangle");
+        alert("error");
       }
 }
